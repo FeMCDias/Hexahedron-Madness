1a93d77ff3dbc819ceefaa0fd4a6631d6f4df0c5
FeMCDias <femcdias@gmail.com>
2023-04-11 18:03:33 -0300

    feat: music added and played

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pg.display.set_caption("Hexahedron Madness!")
     icon = pg.image.load(os.path.join('assets','images','cube_icon.jpg'))
     pg.display.set_icon(icon)
+    music = pg.mixer.music.load(os.path.join('assets','music','Shape Da Future.mp3'))
+    pg.mixer.music.set_volume(0.05)
+    pg.mixer.music.play(-1)
     cube = Hexahedron.Cube(screen)
     cube.run()
 
