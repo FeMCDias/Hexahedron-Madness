0e95851e05641bb55a20cf275942877aa5f23aa1
FeMCDias <femcdias@gmail.com>
2023-04-11 17:57:26 -0300

    docs: Image docs added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame as pg
 import Hexahedron
+import os
 
 def main():
     pg.init()
     screen = pg.display.set_mode((720, 600))
-    pg.display.set_caption("3D Cube")
+    pg.display.set_caption("Hexahedron Madness!")
+    icon = pg.image.load(os.path.join('assets','images','cube_icon.jpg'))
+    pg.display.set_icon(icon)
     cube = Hexahedron.Cube(screen)
     cube.run()
 
